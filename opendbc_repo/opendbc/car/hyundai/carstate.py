ae8d83074d66d8a985b8e83aaa8e826ba9645467
kirikill <133002421+kirikill@users.noreply.github.com>
2025-06-25 19:41:04 +0900

    Update carstate.py

--- opendbc_repo/opendbc/car/hyundai/carstate.py
+++ opendbc_repo/opendbc/car/hyundai/carstate.py
@@ -234,7 +234,7 @@ class CarState(CarStateBase):
     # ------------------------------------------------------------------------
     # custom
 
-    ret.blockPcmEnable = False
+    #ret.blockPcmEnable = False
 
     self.cruise_unavail_cnt += 1 if cp.vl["TCS13"]["CF_VSM_Avail"] != 1 and cp.vl["TCS13"]["ACCEnable"] != 0 else -self.cruise_unavail_cnt
     self.brake_error = self.cruise_unavail_cnt > 100
@@ -379,7 +379,7 @@ class CarState(CarStateBase):
     # ------------------------------------------------------------------------
     # custom messages
 
-    ret.blockPcmEnable = False
+    #ret.blockPcmEnable = False
 
     prev_lfa_btn = self.lfa_btn
     self.lfa_btn = cp.vl[self.cruise_btns_msg_canfd]["LDA_BTN"]
